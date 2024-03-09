3a2e7cd078d116c10b4b5786c297ea24dcaeae73
amrakun <amraa.888@gmail.com>
2024-03-09 20:13:51 +0900

    first commit

---
+++ src/test/java/org/example/hellogrpc/HelloGrpcApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.hellogrpc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloGrpcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
